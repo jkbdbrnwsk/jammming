27728a96437009b341026369662334e0d9a04003
Jakub Dobranowski <jakub@dobranowski.pl>
2020-09-29 17:36:36 +0200

    Implement initial project structure and styles

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './TrackList.css';
+
+import Track from '../Track/Track';
+
+class TrackList extends React.Component {
+  render() {
+    return (
+      <div class='TrackList'>
+        {/* map method that renders a set of Track components */}
+      </div>
+    );
+  }
+}
+
+export default TrackList;
