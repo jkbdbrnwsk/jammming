71ef34614d2f438c94eb31d3b07c882cb9703aba
Jakub Dobranowski <jakub@dobranowski.pl>
2020-10-15 23:57:05 +0200

    Implement adding Tracks to a Playlist

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,8 +2,22 @@ import React from 'react';
 import './Track.css';
 
 class Track extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.addTrack = this.addTrack.bind(this);
+  }
+
   renderAction() {
-    return this.props.isRemoval ? '+' : '-';
+    return this.props.isRemoval ?
+    <button className='Track-action'>-</button>
+    :
+    <button className='Track-action'
+            onClick={this.addTrack}>+</button>;
+  }
+
+  addTrack() {
+    this.props.onAdd(this.props.track);
   }
 
   render() {
@@ -15,7 +29,7 @@ class Track extends React.Component {
             {this.props.track.artist} | {this.props.track.album}
           </p>
         </div>
-        <button className='Track-action'>{this.renderAction()}</button>
+        {this.renderAction()}
       </div>
     );
   }
