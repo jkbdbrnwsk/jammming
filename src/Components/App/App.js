cec136dbef55be7f8f72045cc284d6e17c298a43
Jakub Dobranowski <jakub@dobranowski.pl>
2020-10-16 20:11:58 +0200

    Implement removing Tracks from a Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -54,6 +54,7 @@ class App extends React.Component {
     };
 
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track) {
@@ -64,6 +65,13 @@ class App extends React.Component {
     }
 
     tracks.push(track);
+    this.setState({playlistTracks: tracks});
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id)
+
     this.setState({playlistTracks: tracks});
   }
 
@@ -79,7 +87,8 @@ class App extends React.Component {
             <SearchResults searchResults={this.state.searchResults}
                            onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName}
-                      playlistTracks={this.state.playlistTracks} />
+                      playlistTracks={this.state.playlistTracks}
+                      onRemove={this.state.removeTrack} />
           </div>
         </div>
       </div>
