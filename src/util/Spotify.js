348bd1c1331ff1dbe9d06d1cc0ed9351678002c5
Jakub Dobranowski <jakub@dobranowski.pl>
2020-10-17 18:35:21 +0200

    Add logic for obtaining Spotify access token

---
+++ src/util/Spotify.js
@@ -0,0 +1,30 @@
+const clientId = process.env.SPOTIFY_CLIENT_ID;
+const redirectUri = process.env.REDIRECT_URI;
+
+let accessToken;
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    }
+
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+
+      return accessToken;
+    } else {
+      const accesUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      window.location = accesUrl;
+    }
+  }
+}
+
+export default Spotify;
